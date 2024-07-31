c73850e06be908866567a8161e98df806fe2ab30
Phillip Eduardo Pimenta Forte <phillippimenta@gmail.com>
2024-07-30 22:14:47 -0300

    Finalização da Fase 2

--- README.md
+++ README.md
@@ -1,14 +1,48 @@
 # Tech Challenge - Pós-Tech SOAT - FIAP
 
-Trabalho de conclusão da minha pós-graduação em Arquitetura de Software pela FIAP e Alura - turma 7SOAT.
+Trabalho de conclusão da pós-graduação em Arquitetura de Software pela FIAP e Alura - Turma 7SOAT.
 
-## Membros do grupo 88:
-phillip eduardo pimenta forte - phillippimenta@gmail.com
+## Membros do Grupo 88
+- **Phillip Eduardo Pimenta Forte** - phillippimenta@gmail.com
 
-## Link do Miro:
+## Recursos do Projeto
+
+### Link do Miro
 [https://miro.com/app/board/uXjVNTlNjME=/](https://miro.com/app/board/uXjVKMjQsV4=/)
 
-## Link do vídeo no Youtube:
+### Link do Vídeo no YouTube
+(Adicionar link aqui)
 
-## Diagrama arquiteturais
-![](https://github.com/phillippimenta/pos-tech-fiap-alura/blob/main/documentacao/imagens/group-88-fiap-7soat-techchallenge-diagrams.png)
+## Tecnologias Utilizadas
+
+### Linguagem
+- **Java** - JDK 17
+
+### Banco de Dados
+- **MariaDB** - Versão 10.11
+
+### Frameworks e Ferramentas
+- **Spring Boot**
+- **Springdoc-openapi**
+- **Flyway**
+
+## Diagramas Arquitetônicos
+![Diagramas Arquitetônicos](https://github.com/phillippimenta/pos-tech-fiap-alura/blob/main/documentacao/imagens/group-88-fiap-7soat-techchallenge-diagrams.png)
+
+## Execução da Aplicação Localmente
+
+### Utilizando Kubernetes (K8s)
+1. **Clone o repositório:**
+   ```bash
+   git clone https://github.com/phillippimenta/pos-tech-fiap-alura
+   
+### Instale o Minikube
+Siga as instruções no site oficial: [Minikube Installation](https://minikube.sigs.k8s.io/docs/start/)
+
+1. **Inicie o Minikube**
+   ```bash
+   minikube start
+
+2. **Execute o script de deploy**
+   ```bash
+   ./deploy_minikube.sh
--- deploy_minikube.sh
+++ deploy_minikube.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
-
-# Nome do namespace (opcional, ajuste se necessário)
-NAMESPACE="techchallenge"
-
-# Nome da imagem Docker e tag
-IMAGE_NAME="techchallenge-app"
-IMAGE_TAG="latest"
-
-# Configura o ambiente Docker do Minikube
-eval $(minikube docker-env)
-
-# Construir a imagem Docker
-echo "Construindo a imagem Docker..."
-docker build -t $IMAGE_NAME:$IMAGE_TAG .
-
-# Aplicar configurações Kubernetes
-echo "Aplicando configurações Kubernetes..."
-kubectl apply -f k8s/db-deployment.yaml -n $NAMESPACE
-kubectl apply -f k8s/db-service.yaml -n $NAMESPACE
-kubectl apply -f k8s/app-deployment.yaml -n $NAMESPACE
-kubectl apply -f k8s/app-service.yaml -n $NAMESPACE
-
-# Verificar o status dos pods
-echo "Verificando o status dos pods..."
-kubectl get pods -n $NAMESPACE
-
-# Verificar o status dos serviços
-echo "Verificando o status dos serviços..."
-kubectl get services -n $NAMESPACE
-
-# Exibir logs dos pods para diagnóstico
-echo "Exibindo logs dos pods..."
-kubectl logs -l app=$IMAGE_NAME -n $NAMESPACE
+echo 'Removendo namespace "techchallenge" se já existir...'
+kubectl delete namespace techchallenge --ignore-not-found
+echo 'Criando namespace "techchallenge"...'
+kubectl create namespace techchallenge
+sleep 5
+echo 'Namespace "techchallenge" criado com sucesso.'
+echo 'Iniciando configuração do banco de dados...'
+kubectl apply -f k8s/db-deployment.yaml -n techchallenge
+sleep 5
+echo 'Deployment do banco de dados aplicado com sucesso.'
+echo 'Configurando serviço para o banco de dados...'
+kubectl apply -f k8s/db-service.yaml -n techchallenge
+sleep 5
+echo 'Serviço do banco de dados configurado com sucesso.'
+echo 'Configurando serviço para a aplicação...'
+kubectl apply -f k8s/app-service.yaml -n techchallenge
+sleep 5
+echo 'Iniciando deployment da aplicação...'
+kubectl apply -f k8s/app-deployment.yaml -n techchallenge
+sleep 60
+echo 'Deployments concluídos.'
